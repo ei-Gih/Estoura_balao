f9bcc4e91df84bbcf86e0c0ae50392b30c244e87
Gisele Lais <105565774+ei-Gih@users.noreply.github.com>
2024-09-23 21:04:08 -0300

    Ajusteis

--- jogo.js
+++ jogo.js
@@ -1,110 +1,94 @@
-var timerId = null; //variável que armazena a chamada da função timeout
+var timerId = null;
 
-function iniciaJogo(){
+function iniciaJogo() {
+  var url = window.location.search;
+  var nivel_jogo = url.replace("?", "");
+  var tempo_segundos = 0;
 
-	var url = window.location.search;
-	
-	var nivel_jogo = url.replace("?", "");
+  if (nivel_jogo == 1) {
+    tempo_segundos = 120;
+  } else if (nivel_jogo == 2) {
+    tempo_segundos = 60;
+  } else if (nivel_jogo == 3) {
+    tempo_segundos = 30;
+  }
 
-	var tempo_segundos = 0;
+  document.getElementById("cronometro").innerHTML = tempo_segundos;
+  var qtde_baloes = 80;
+  cria_baloes(qtde_baloes);
+  document.getElementById("baloes_inteiros").innerHTML = qtde_baloes;
+  document.getElementById("baloes_estourados").innerHTML = 0;
 
-	if(nivel_jogo == 1) { //1 fácil -> 120segundos
-		tempo_segundos = 120;
-	}
-
-	if(nivel_jogo == 2) { //2 normal -> 60segundos
-		tempo_segundos = 60;
-	}
-	
-	if(nivel_jogo == 3) { //3 difícil -> 30segundos
-		tempo_segundos = 30;
-	}
-
-	//inserindo segundos no span
-	document.getElementById('cronometro').innerHTML = tempo_segundos;
-
-	// quantidade de balões
-	var qtde_baloes = 78;
-	
-	cria_baloes(qtde_baloes);
-
-	//imprimir qtde baloes inteiros
-	document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
-	document.getElementById('baloes_estourados').innerHTML = 0;
-
-	contagem_tempo(tempo_segundos + 1)
-	
+  contagem_tempo(tempo_segundos + 1);
 }
 
-function contagem_tempo(segundos){
+function contagem_tempo(segundos) {
+  segundos = segundos - 1;
 
-	segundos = segundos - 1;
+  if (segundos == -1) {
+    clearTimeout(timerId);
+    game_over();
+    return false;
+  }
 
-	if(segundos == -1){
-		clearTimeout(timerId); //para a execução da função do settimeout
-		game_over();
-		return false;
-	}
-
-	document.getElementById('cronometro').innerHTML = segundos;
-
-	timerId = setTimeout("contagem_tempo("+segundos+")", 1000);
+  document.getElementById("cronometro").innerHTML = segundos;
+  timerId = setTimeout(function () {
+    contagem_tempo(segundos);
+  }, 1000);
 }
 
-function game_over(){
-	alert('Fim de jogo, você não conseguiu estourar todos os balões a tempo');
+function game_over() {
+  alert("Fim de jogo, você não conseguiu estourar todos os balões a tempo");
 }
 
-function cria_baloes(qtde_baloes){
+function cria_baloes(qtde_baloes) {
+  var cenario = document.getElementById("cenario");
+  var larguraTela = window.innerWidth * 0.8;
+  var larguraBalao = 100; // incluindo margens
+  var colunas = Math.floor(larguraTela / larguraBalao);
+  var linhas = Math.ceil(qtde_baloes / colunas);
 
-	for(var i = 1; i <= qtde_baloes; i++){
-
-		var balao = document.createElement("img");
-		balao.src = 'imagens/balao-roxo.png';
-		balao.style.margin = '10px';
-		balao.style.width = '40px';
-		balao.id = 'b'+i;
-		balao.onclick = function(){ estourar(this); };
-
-		document.getElementById('cenario').appendChild(balao);
-	}
+  for (var i = 1; i <= qtde_baloes; i++) {
+    var balao = document.createElement("img");
+    balao.src = "imagens/balao-roxo.png";
+    balao.id = "b" + i;
+    balao.onclick = function () {
+      estourar(this);
+    };
+    cenario.appendChild(balao);
+  }
 }
 
-function estourar(e){
-
-	var id_balao = e.id;
-
-	document.getElementById(id_balao).setAttribute("onclick", "");
-	document.getElementById(id_balao).src = 'imagens/balao-roxo-estourado.png';
-
-	pontuacao(-1);
-
+function estourar(e) {
+  e.onclick = null;
+  e.classList.add("estourado");
+  pontuacao(-1);
 }
 
-function pontuacao(acao){
+function pontuacao(acao) {
+  var baloes_inteiros = parseInt(
+    document.getElementById("baloes_inteiros").innerHTML
+  );
+  var baloes_estourados = parseInt(
+    document.getElementById("baloes_estourados").innerHTML
+  );
 
-	var baloes_inteiros = document.getElementById('baloes_inteiros').innerHTML;
-	var baloes_estourados  = document.getElementById('baloes_estourados').innerHTML;
+  baloes_inteiros += acao;
+  baloes_estourados -= acao;
 
-	baloes_inteiros = parseInt(baloes_inteiros);
-	baloes_estourados = parseInt(baloes_estourados);
+  document.getElementById("baloes_inteiros").innerHTML = baloes_inteiros;
+  document.getElementById("baloes_estourados").innerHTML = baloes_estourados;
 
-	baloes_inteiros = baloes_inteiros + acao;
-	baloes_estourados = baloes_estourados - acao;
-
-	document.getElementById('baloes_inteiros').innerHTML = baloes_inteiros;
-	document.getElementById('baloes_estourados').innerHTML = baloes_estourados;
-
-	situacao_jogo(baloes_inteiros);
+  situacao_jogo(baloes_inteiros);
 }
 
-function situacao_jogo(baloes_inteiros){
-	if(baloes_inteiros == 0){
-		alert('Parabéns, você conseguiu estourar todos os balões a tempo');
-		parar_jogo();
-	}
+function situacao_jogo(baloes_inteiros) {
+  if (baloes_inteiros === 0) {
+    alert("Parabéns, você conseguiu estourar todos os balões a tempo!");
+    parar_jogo();
+  }
 }
 
-function parar_jogo(){
-	clearTimeout(timerId);
+function parar_jogo() {
+  clearTimeout(timerId);
 }
